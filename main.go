536e33f9516c494cba3c25e119d2eaf7e9c290e3
Stephen Horvath <stephen.horvath30@gmail.com>
2023-11-19 11:24:48 +0000

    sorting the astronauts

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sort"
 )
 
 type Astronaut struct {
@@ -12,6 +13,20 @@ type Astronaut struct {
 	Craft string
 }
 
+// sort the astronaughts. if cbn (craft by name) is true will be sorted by craft then name
+// else sorted by name then craft
+func sortAstronauts(ast []Astronaut, cbn bool) {
+	sort.Slice(ast, func(i, j int) bool {
+		if ast[i].Craft == ast[j].Craft && cbn || ast[i].Name != ast[j].Name && !cbn {
+			return ast[i].Name < ast[j].Name
+		} else {
+			return ast[i].Craft < ast[j].Craft
+		}
+	})
+}
+
+// extracts a slice of Astronaut. The supplied byte slice is assumed to represent
+// valid json with a key "people" containint he astronauts
 func extractAstronauts(astroJson []byte) []Astronaut {
 	type Astronauts struct {
 		People []Astronaut
@@ -35,7 +50,7 @@ func fetch(url string) []byte {
 			panic(err)
 		}
 	}()
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != 201 {
 		panic(fmt.Sprintf("Houston we have a problem %s returned with status %d", url, resp.StatusCode))
 	}
 	body, err := io.ReadAll(resp.Body)
@@ -47,7 +62,9 @@ func fetch(url string) []byte {
 
 func main() {
 	const url string = "http://api.open-notify.org/astros.json"
+	const cbn bool = false
 	astroJson := fetch(url)
 	astronauts := extractAstronauts(astroJson)
+	sortAstronauts(astronauts, cbn)
 	fmt.Println(astronauts)
 }
